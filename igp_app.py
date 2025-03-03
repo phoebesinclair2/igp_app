669224f23eb9deecc5ae739de1dc59498bca700d
Phoebe Sinclair <152397577+phoebesinclair2@users.noreply.github.com>
2025-03-03 22:51:10 +0000

    Add files via upload

---
+++ igp_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.title("My First Streamlit App")
+st.write("Hello, Streamlit!")
+
+# Bash command to run app: run "'/Users/phoebesinclair/Library/CloudStorage/OneDrive-Personal/My Documents/GitHub/igp_app' streamlit repo/app.py"
